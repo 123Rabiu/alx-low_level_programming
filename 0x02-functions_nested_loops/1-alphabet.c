1c02718500bdec0c2c79df61f8c2ccfb72ee709c
RABIU_MUHAMMAD_RABIU <rrabiu389@gmail.com>
2023-02-22 17:44:08 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print alphabet -> orints the lower case alphabets
+ */
+void print _alphabet(void)
+{
+	int j;
+
+	for (j = 'a'; j <= 'z' ; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
